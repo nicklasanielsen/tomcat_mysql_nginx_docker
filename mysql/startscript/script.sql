d6d69065e203607f539c31acf2014ed333ac5537
Thomas-Hartmann <thomas@webtrade.dk>
2020-08-03 15:40:55 +0200

    database startscript added

--- mysql/startscript/script.sql
+++ mysql/startscript/script.sql
@@ -1,4 +1,16 @@
-USE db;
+CREATE DATABASE startcode;
+CREATE DATABASE startcode_test;
+CREATE DATABASE dev_db;
+
+# create root user and grant rights
+CREATE USER 'dev'@'%' IDENTIFIED BY 'ax2';
+GRANT ALL ON *.* TO 'dev'@'%';
+
+-- GRANT ALL PRIVILEGES ON startcode.* TO 'dev'@'%';
+-- GRANT ALL PRIVILEGES ON startcode_test.* TO 'dev'@'%';
+-- GRANT ALL PRIVILEGES ON db.* TO 'dev'@'%';
+
+USE dev_db;
 CREATE TABLE t1 (
     c1 INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     c2 VARCHAR(100),
